3f0c30e1d5b7950f6708958a730abf334b916198
Avirup Biswas <avirupbiswas82@gmail.com>
2025-07-13 22:30:46 +0530

    New project to push to GitHub

---
+++ src/pages/Favorite.jsx
@@ -0,0 +1,9 @@
+import "../css/Favorites.css";
+function Favorite() {
+    return <div className="favorite-empty">
+        <h2>No favorite Movies yet</h2>
+        <p>start adding movies to your favorites and they will appear here!</p>
+    </div>
+}
+
+export default Favorite
